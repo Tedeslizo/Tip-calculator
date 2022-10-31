6540acc77842defbeacc6525aeb03e1e20f0f5e5
Tedeslizo <116922467+Tedeslizo@users.noreply.github.com>
2022-10-31 23:29:55 +0000

    lo deje de pana

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 
 print("Welcome to the tip calculator my bro\n ")
 total = float(input("What was the total bill $?\n "))
-percentage_chosen = int(input("What percentage tip would you like to give? 10, 12, or 15?\n "))
+percentage_chosen = int(input("What percentage tip would you like to give?\n "))
 people_who_ate = int(input("How many people to split the bill? \n"))
 tip_to_give_per_person = (float(total) / 100) * float(percentage_chosen) / int(people_who_ate)
 total_sin_propina = float(total) / int(people_who_ate)
 total_con_propina = percentage_chosen / 100 * total + total
 bill_per_person = total_con_propina / people_who_ate
-final_amount = round(bill_per_person, 2)
+final_amount = round(bill_per_person)
 print(f"Each person should pay ${final_amount} ")
 print("No seas laji y siempre se educado con la persona que te atiende")
+input("ah lo necesitay sin propina? ")
+print(float(total) / int(people_who_ate))
+print("de nada mi bro ocupame cuando gustes")
